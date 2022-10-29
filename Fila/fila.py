afdcd38274bbafc9838383d79ae34dc8243a3249
marx3512 <marxborgesmachado@gmail.com>
2022-10-29 14:11:12 -0300

    adicionando nova versao da fila

--- Fila/fila.py
+++ Fila/fila.py
@@ -1,72 +1,77 @@
-from multiprocessing import Semaphore
-import random
-import threading
-import time
+# import random
+# import threading
+# import time
 
-semaforo = threading.Semaphore(3)
+# semaforo = threading.Semaphore(3)
 
-class Nodo:
-    def __init__(self, dado = 0, proximoNodo = None):
-        self.dado = dado
-        self.proximo = proximoNodo
+# def removendo():
+#     semaforo.acquire()
+#     
+#     semaforo.release()
 
-    def __repr__(self):
-        return '%s -> %s' % (self.dado, self.proximo)
 
-class Fila:
-    def __init__(self):
-        self.primeiro = None
-        self.ultimo = None
+# t1 = threading.Thread(target=removendo)
+# t2 = threading.Thread(target=removendo)
+# t3 = threading.Thread(target=removendo)
+
+# t1.start()
+# t2.start()
+# t3.start()
+
+# t1.join()
+# t2.join()
+# t3.join()
+
+
+
+class MyCircularQueue():
+    def __init__(self,k):
+        self.k = k
+        self.queue = [None] * k
+        self.head = self.tail = -1
     
-    def __repr__(self):
-        return "[" + str(self.primeiro) + "]"
-
-    def inserir(self, novoDado):
-        novoNodo = Nodo(novoDado)
-
-        if self.primeiro == None:
-                self.primeiro = novoNodo
-                self.ultimo = novoNodo
+    def insertElement(self, data):
+        if ((self.tail + 1) % self.k == self.head):
+            print("A fila circular esta cheia\n")
+        elif (self.head == -1):
+            self.head = 0
+            self.tail = 0
+            self.queue[self.tail] = data
         else:
-            self.ultimo.proximo = novoNodo
-            self.ultimo = novoNodo
-        
+            self.tail = (self.tail + 1) % self.k
+            self.queue[self.tail] = data
+    
+    def deleteElement(self):
+        if(self.head == -1):
+            print("A fila circular esta vazia/n")
+        elif (self.head == self.tail):
+            temp = self.queue[self.head]
+            self.head = -1
+            self.tail = -1
+            return temp
+        else:
+            temp = self.queue[self.head]
+            self.head = (self.head + 1) % self.k
+            return temp
+    
+    def showQueue(self):
+        if(self.head == 1):
+            print("Nao tem elementos na fila")
+        elif (self.tail >= self.head):
+            for i in range(self.head, self.tail + 1):
+                print(self.queue[i], end = " ")
+            print()
+        else:
+            for i in range(self.head, self.k):
+                print(self.queue[i], end = " ")
+            for i in range(0, self.tail + 1):
+                print(self.queue[i], end = " ")
+            print()
 
-    def remove(self):
-        self.primeiro = self.primeiro.proximo
-
-        if self.primeiro == None:
-            self.ultimo = None
-
-fila = Fila()
-print("Fila vazia: ", fila)
+obj = MyCircularQueue(10)
 
 for i in range(10):
-    fila.inserir(i)
-    print("Insere o valor {0} final na fila: {1}".format(i, fila))
+    obj.insertElement(i)
 
-while fila.primeiro != None:
-    fila.remove()
-    time.sleep(random.randint(3,4))
-    print("Removendo elemento que está no começo da fila: ", fila)
-
-def removendo():
-    semaforo.acquire()
-    while fila.primeiro != None:
-        fila.remove()
-        time.sleep(random.randint(3,4))
-        print("Removendo elemento que está no começo da fila: ", fila)
-    semaforo.release()
-
-
-t1 = threading.Thread(target=removendo)
-t2 = threading.Thread(target=removendo)
-t3 = threading.Thread(target=removendo)
-
-t1.start()
-t2.start()
-t3.start()
-
-t1.join()
-t2.join()
-t3.join()
+print("Fila inicial")
+obj.showQueue()
