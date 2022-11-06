a94fde7d7f86ebf3c5b0cac7a39557f8bcb1dca6
DJJJonas <jonasfilho73@gmail.com>
2022-11-05 21:47:17 -0300

    refactor dinamic circular queue

--- Fila/fila.py
+++ Fila/fila.py
@@ -1,46 +1,63 @@
-import threading
-import random
-from typing import Any, List
+from typing import Any, Optional
 
-semaforo = threading.Semaphore(3)
 
-class MyCircularQueue():
+class Node:
+    def __init__(self, element: Any=None) -> None:
+        self.element: Any = element
+        self.next: Optional[Node, None] = None  # type: ignore
+        self.last: Optional[Node, None] = None  # type: ignore
+
+
+class CircularQueue:
     def __init__(self, maxlength: int):
         self.maxlength: int = maxlength
         self.length: int = 0
-        self.queue: List[Any] = [None] * maxlength
+        self.root: Optional[Node, None] = None  # type: ignore
 
-    def insertElement(self, element: Any):
+    def insert_element_start(self, element: Any):
+        self.insert_element_end(element)
+        self.root = self.root.last
+
+    def insert_element_end(self, element: Any):
         if self.length == self.maxlength:
-            print('fila circular cheia')
             return
-        self.queue[self.length] = element
         self.length += 1
-
-    def deleteFirstElement(self):
         if self.length == 0:
-            print('fila circular vazia')
+            self.root = Node(element)
+            self.root.next = self.root
+            self.root.last = self.root
             return
-        self.queue.remove(self.queue[0])
-        self.length -= 1
+        node = Node(element)
+        self.root.last.next = node
+        node.last = self.root.last
+        node.next = self.root
+        self.root.last = node
+
+    def delete_first_element(self):
+        self.length -=1
+        if self.length == 1:
+            self.root = None
+        self.root.last.next = self.root.next
+        self.root.next.last = self.root.last
+        self.root = self.root.next
 
     def __repr__(self) -> str:
-        result: str = ''
-        for element in self.queue:
-            if element == None:
-                break
-            result += f'{element} '
-        return result
+        node = self.root
+        elements = []
+        for i in range(self.length):
+            elements.append(node.element)
+            node = node.next
+        return str(elements)
 
 
-obj = MyCircularQueue(11)
+queue = CircularQueue(11)
 
-for i in range(10):
-    obj.insertElement(random.randint(1,100))
-
-print("Fila inicial")
-print(obj)
-obj.deleteFirstElement()
-
-obj.insertElement(random.randint(1,100))
-print(obj)
+print(queue)
+queue.insert_element_end(2)
+print(queue)
+queue.insert_element_end(4)
+print(queue)
+queue.insert_element_start(8)
+print(queue)
+queue.delete_first_element()
+print(queue)
